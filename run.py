bb49fc063eb4362b0e00c22e15387037709a6d78
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-04-13 01:22:26 +0000

    Add Final Score function

--- run.py
+++ run.py
@@ -20,33 +20,45 @@ def new_game():
     player_score = 0
     answer = riddles.col_values(2)
     answer_two = riddles.col_values(3)
-    for riddle in riddles.col_values(1):
-        A = answer[riddle_number]
-        B = answer_two[riddle_number]
-        print("*******************************")
-        print(riddle)
-        print()
-        print("A: ", A,"  ", "B: ", B)
-        print()        
-        player_guess = input("Enter (A, B): ")
-        print()
-        player_guess = player_guess.upper()
-        player_guesses.append(player_guess)
-        riddle_number += 1
-        player_score += answer_check(correct[riddle_number], player_guess)
-        print(player_score)
+    while riddle_number < 5:
+        for riddle in riddles.col_values(1):
+            A = answer[riddle_number]
+            B = answer_two[riddle_number]
+            print("*******************************")
+            print(riddle)
+            print()
+            print("A: ", A,"  ", "B: ", B)
+            print()        
+            player_guess = input("Enter (A, B): ")
+            print()
+            player_guess = player_guess.upper()
+            player_guesses.append(player_guess)
+            player_score += answer_check(correct[riddle_number], player_guess)
+            print(player_score)
+            riddle_number += 1
+            print(riddle_number)
+    final_score(player_score)
+
 
 def answer_check(correct_answer, player_guess):
 
     if correct_answer == player_guess:
         print("You Answered Correct!")
+        print()
         return 1
     else:
         print("Sorry Wrong Answer!")
+        print()
         return 0
 
-def final_score():
-    pass
+def final_score(player_score):
+    print("*******************************")
+    print("Your Final Result")
+    print()
+    final_score = int((player_score/len(correct))*100)
+    print(f"You Answered: {player_score} Correct")
+    print()
+    print("Your score is: "+str(final_score)+"%")
 
 
 def store_score():
@@ -55,5 +67,5 @@ def store_score():
 def play_again():
     pass
 
-new_game()
 
+new_game()
