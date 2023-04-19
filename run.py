65266d7c526412b2f027c329a9a240ea5a817a1a
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-04-19 18:19:10 +0000

    Add styles to out puts and cleaned up code

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
 import random
-import gspread
+
+"""Rich imports"""
+from rich import box
 from rich.table import Table
 from rich.console import Console
+
+"""Imports for google sheets and gspread"""
+import gspread
 from google.oauth2.service_account import Credentials
 
+"""Google sheet variables and url links"""
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -17,7 +22,17 @@ SHEET = GSPREAD_CLIENT.open('Riddles')
 riddles = SHEET.worksheet("riddles")
 score_list = SHEET.worksheet("score")
 correct = riddles.col_values(5)
+
+"""Variables for styling the console out puts """
+correct_style = "spring_green3 on grey0"
+incorrect_style = "blink red on grey0"
+lines_style = "bold bright_red on grey0"
+info_style = "bright_yellow on grey0"
+riddle_style = "white on grey0"
+option_style = "wheat1 on grey0"
 console = Console()
+
+"""Global variables to hold game data"""
 player = ""
 percentage = 0
 player_score = 0
@@ -26,61 +41,57 @@ nums = []
 list_score = []
 
 
+def player_details():
+    """ 
+    Welcomes the user and asks for there name and saves to the player_name variable
+    """
+    global player
+    console.print(" \n Welcome to Riddles.\n ", style=info_style, justify="center")
+    console.print("------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+    console.print("Please Type Your Name \n ", style=info_style, justify="center")
+    player_name = input()
+    player = player_name
+
+
+def welcome():
+    """
+    Displays information to the user about the game.
+    """
+
+    console.print(" \n ------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+    console.print(f"Hello {player}!!  \n ", style=info_style, justify="center")
+    console.print("How to Play  \n ", style=info_style, justify="center")
+    console.print("------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+    console.print("* There are 20 riddles to be answered \n ", style=info_style, justify="center")
+    console.print("* Each riddle has 3 options A, B or C to choose from  \n ", style=info_style, justify="center")
+    console.print("* Each riddle will give you 1 point \n ", style=info_style, justify="center")
+    console.print("------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+    console.print("Good Luck \n ", style=info_style, justify="center")
+
 def diplay_score():
 
     global list_score
     numx = 1
     score_lists = SHEET.worksheet("score").get_all_values()
-    print("*******************************")
-    print()
-    print("To See Saved Scores")
-    print("Type 'Yes'")
-    print("Or Press Any Key To Continue")
-    print()
+    console.print(" \n ------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+    console.print("To See Saved Scores \n ", style=info_style, justify="center")
+    console.print("Type 'Yes' \n ", style=info_style, justify="center")
+    console.print("Or Press Any Key To Continue \n ", style=info_style, justify="center")
     scores = input()
     scores = scores.upper()
     if scores == "YES":
         list_score = score_list.row_values(numx)
         numx += 1
-        table = Table(show_header=True, header_style="bold red on black", style="red on black", box=None)
-        table.add_column(list_score[0], justify="center", width=16,style="red on black")
-        table.add_column(list_score[1], justify="center", width=16,style="red on black")
-        table.add_column(list_score[2], justify="center", width=16,style="red on black")
+        table = Table(title=" \n Scores \n", title_justify="center", title_style="yellow on grey0", show_header=True, header_style="blue3 on grey0", style="red on grey0", box=box.ASCII, expand=True)
+        table.add_column(list_score[0], justify="center", width=16, style="spring_green3 on grey0")
+        table.add_column(list_score[1], justify="center", width=16, style="spring_green3 on grey0")
+        table.add_column(list_score[2] + "%", justify="center", width=16, style="spring_green3 on grey0")
         while numx <= len(score_lists):
             list_score = score_list.row_values(numx)
             table.add_row(list_score[0], list_score[1], list_score[2],)
             numx += 1
         console.print(table)
-
-
-def player_details():
-
-    global player
-    print("Welcome to Riddles.")
-    print()
-    print("*******************************")
-    print()
-    print("Please Type Your Name")
-    player_name = input()
-    print()
-    player = player_name
-
-
-def welcome():
-
-    print("*******************************")
-    print()
-    print(f"Hello {player}!!")
-    print()
-    print("How to Play")
-    print("-----------------------------------------------------")
-    print("* There are 20 riddles to be answered")
-    print("* Each riddle has 3 options A, B or C to choose from")
-    print("* Each riddle will give you 1 point")
-    print("-----------------------------------------------------")
-    print()
-    print("Good Luck")
-    print()
+    welcome()
 
 
 def play():
@@ -93,20 +104,15 @@ def play():
         if num not in nums:
             nums.append(num)
             question = riddles.row_values(num)
-            print("*******************************")
-            print()
-            print(f"Riddle Number {riddle_number}")
-            print()
-            print(question[0])
-            print()
-            print(f"A: {question[1]} B: {question[2]} C: {question[3]}")
-            print() 
+            console.print(" \n ------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+            console.print(f"Riddle Number {riddle_number} \n ", style=info_style, justify="center")
+            console.print(f"{question[0]} \n" , style=riddle_style, justify="center")
+            console.print(f"A: {question[1]}      B: {question[2]}    C: {question[3]}  \n ", style=option_style, justify="center")
             count = 0
             player_guess = ""
             while count == 0:
                 player_guess = input("Enter (A, B, C): ")
                 player_guess = player_guess.upper()
-                print()
                 if player_guess == "A":
                     count += 1
                 elif player_guess == "B":
@@ -114,14 +120,11 @@ def play():
                 elif player_guess == "C":
                     count += 1
                 else:
-                    print("*******************************")
-                    print()
-                    print("Incorrect Value!!")
-                    print()       
-            print()
+                    console.print("------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+                    console.print("Incorrect Value!! :warning:  \n ", style=incorrect_style, justify="center")
+                    console.print("Type A, B, or C  \n ", style=incorrect_style, justify="center")  
             player_score += answer_check(question[4], player_guess)
-            print(f"Your Current Score Is {player_score}")
-            print()
+            console.print(f"Your Current Score Is {player_score}  \n ", style=info_style, justify="center")
             riddle_number += 1
         
     final_score(player_score)
@@ -133,45 +136,38 @@ def answer_check(correct_answer, player_guess):
 
     global question
     if correct_answer == player_guess:
-        print("You Answered Correct!")
-        print()
+        console.print(" \n You Answered Correct!  \n ", style=correct_style, justify="center")
         return 1
     else:
-        print("Sorry Wrong Answer!")
-        print()
+        console.print(" \n Sorry Wrong Answer!  \n ", style=incorrect_style, justify="center")
         return 0
 
 
 def final_score(player_score):
 
     global percentage
-    print("*******************************")
-    print()
-    print("Your Final Result")
-    print()
+    console.print("------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+    console.print("Your Final Result  \n ", style=info_style, justify="center")
     percentage = int((player_score/len(correct))*250)
-    print(f"You Answered: {player_score} Riddles Corretly with "+str(percentage)+"% Accuracy")
-    print()
+    console.print(f"You Answered: {player_score} Riddles Corretly with "+str(percentage)+"% Accuracy  \n ", style=correct_style, justify="center")
     if player_score == 50:
-        print(f"CONGRATULATIONS {player} YOU ARE A RIDDLE MASTER")
+        console.print(f"CONGRATULATIONS {player} YOU ARE A RIDDLE MASTER  \n ", style=correct_style, justify="center")
 
 
 def upload_score():
 
     data = [player, player_score, percentage]
-    print("*******************************")
-    print()
-    print("To Save Your Score")
-    print("Type 'Yes'")
-    print("Or Press Any Key To Continue")
-    print()
+    console.print(" \n ------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+    console.print("To Save Your Score  \n ", style=info_style, justify="center")
+    console.print("Type 'Yes'  \n ", style=info_style, justify="center")
+    console.print("Or Press Any Key To Continue  \n ", style=info_style, justify="center")
     upload = input()
     upload = upload.upper()
     if upload == "YES":
-        print("Uploading Score\n")
+        console.print("Uploading Score \n", style=info_style, justify="center")
         score_worksheet = SHEET.worksheet("score")
         score_worksheet.append_row(data)
-        print("Successfully added.\n")
+        console.print("Successfully added. \n", style=info_style, justify="center")
 
 
 def play_again():
@@ -181,12 +177,10 @@ def play_again():
     global question
     global nums
     global list_score
-    print("*******************************")
-    print()
-    print("To Play Again")
-    print("Type 'Yes'")
-    print("Or Press Any Key To Continue")
-    print()
+    console.print("------------------------------------------------------------------------- \n ", style=lines_style, justify="center")
+    console.print("To Play Again  \n ", style=info_style, justify="center")
+    console.print("Type 'Yes'  \n ", style=info_style, justify="center")
+    console.print("Or Press Any Key To Continue \n ", style=info_style, justify="center")
     restart = input()
     restart = restart.upper()
     if restart == "YES":
@@ -206,9 +200,8 @@ def new_game():
     play()    
     while play_again():
         play()
-    print("*******************************")    
-    print()
-    print("Thank you for playing")
+    console.print(" \n ------------------------------------------------------------------------- \n ", style=lines_style, justify="center")    
+    console.print("Thank you for playing \n  ", style=info_style, justify="center")
 
 
 new_game()
